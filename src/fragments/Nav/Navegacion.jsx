c52c3feae8c4ee2f7de2920bfb4bb8bb6c62df66
Alexis <220240@fppiramide.com>
2024-05-08 08:53:29 +0200

    No funciona el boton nuevo

--- src/fragments/Nav/Navegacion.jsx
+++ src/fragments/Nav/Navegacion.jsx
@@ -1,9 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import '../../style/Navegacion.css'
 import { useEffect } from "react";
-export default function Nav({usuario,recarga, setRecarga}) {
+import BotonNuevoExamen from "./BotonNuevoExamen";
+export default function Nav({usuario}) {
     const navigate = useNavigate();
 
+    
 
     return ( 
         <nav>
@@ -11,7 +13,8 @@ export default function Nav({usuario,recarga, setRecarga}) {
             <ul>
                 {!usuario ? <li><button onClick={() => navigate('/login')}>Log In</button></li> : ''}
                 {!usuario ? <li><button onClick={() => navigate('/registro')}>Registro</button></li> : ''}
-                {usuario ? <li><button onClick={() => {setRecarga(!recarga); navigate('/examenes')}}>Examenes</button></li> : ''}
+                {usuario ? <li><button onClick={() => navigate('/examenes')}>Examenes</button></li> : ''}
+                {usuario ? <li><BotonNuevoExamen /></li> : ''}
             </ul>
         </nav>
      );
